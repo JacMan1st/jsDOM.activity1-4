59e9dc425cc112763439f7be105fb80e3e227a1c
Jack Mears <jack.mears@ymail.com>
2024-01-09 11:21:51 +0000

    activity 4 complete

--- main.js
+++ main.js
@@ -35,3 +35,11 @@ colourButt.addEventListener("click", () => {
     colourHead.style.color = inputBox.value;
     inputBox.value = "";
 })
+
+// // // ACTIVITY 4 // // //
+document.addEventListener("click", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    const coordsPara = document.getElementById("coordinates");
+    coordsPara.textContent = `You clicked at coordinates X: ${x} and Y: ${y}`;
+});

02e3fcab0c67c5d643cded792ebc5211f6059df4
Jack Mears <jack.mears@ymail.com>
2024-01-09 09:39:13 +0000

    activity 3 complete

--- main.js
+++ main.js
@@ -26,3 +26,12 @@ imgToChange.addEventListener("click", () => {
     inputURL.value = "";
 })
 
+// // // ACTIVITY 3 // // // 
+const colourHead = document.getElementById("colourHead");
+const inputBox = document.getElementById("inputBox");
+const colourButt = document.getElementById("colourButt");
+
+colourButt.addEventListener("click", () => {
+    colourHead.style.color = inputBox.value;
+    inputBox.value = "";
+})
